2487675cd8baec154498e901e51035509b09b24d
sbrevolution5 <Sbrevolution5@aim.com>
2021-02-05 11:22:16 -0500

    added state to form

--- src/containers/form.js
+++ src/containers/form.js
@@ -1,6 +1,90 @@
 const { render } = require("@testing-library/react")
+const { Component } = require("react")
 
-import React { Component } from 'react'
+import React { Component } from 'react';
 
-render(){
-   
+class Form extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            name: '',
+            type: '',
+            stats: {
+                str: 10,
+                dex: 10,
+                con: 10,
+                int: 10,
+                wis: 10,
+                cha: 10
+            },
+            health: '',
+            ac: '',
+            vulnerabilities: {
+                Slashing: false,
+                Piercing: false,
+                Bludgeoning: false,
+                Poison: false,
+                Acid: false,
+                Fire: false,
+                Cold: false,
+                Radiant: false,
+                Necrotic: false,
+                Lightning: false,
+                Thunder: false,
+                Force: false,
+                Psychic: false,
+            },
+            resistances: {
+                Slashing: false,
+                Piercing: false,
+                Bludgeoning: false,
+                Poison: false,
+                Acid: false,
+                Fire: false,
+                Cold: false,
+                Radiant: false,
+                Necrotic: false,
+                Lightning: false,
+                Thunder: false,
+                Force: false,
+                Psychic: false,
+            },
+            immunities: {
+                Slashing: false,
+                Piercing: false,
+                Bludgeoning: false,
+                Poison: false,
+                Acid: false,
+                Fire: false,
+                Cold: false,
+                Radiant: false,
+                Necrotic: false,
+                Lightning: false,
+                Thunder: false,
+                Force: false,
+                Psychic: false,
+            },
+            conditionImmunities:{
+                Blinded: false,
+                Charmed: false,
+                Deafened: false,
+                Exhaustion: false,
+                Frightened: false,
+                Grappled: false,
+                Incapacitated: false,
+                Invisible: false,
+                Paralyzed: false,
+                Petrified: false,
+                Poisoned: false,
+                Prone: false,
+                Restrained: false,
+                Stunned: false,
+                Unconscious: false,
+
+            },
+            traits: {},
+            actions: {},
+            
+        }
+    }
+}
