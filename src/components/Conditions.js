67960397a0c590e2e3c7060f081177551ea146f0
sbrevolution5 <Sbrevolution5@aim.com>
2021-02-05 12:44:37 -0500

    added arrays

---
+++ src/components/Conditions.js
@@ -0,0 +1,9 @@
+import { Component } from "react";
+
+// should be collapsable, with a number to show how many conditions are checked while it is collapsed. 
+class Conditions extends Component{
+    const conditionList = [Blinded, Charmed, Deafened, Exhaustion, Frightened, Grappled, Incapacitated, Invisible, Paralyzed, Petrified, Poisoned, Prone, Restrained, Stunned, Unconscious,]
+    render(){
+
+    }
+}
--- src/components/DamageTypes.js
+++ src/components/DamageTypes.js
@@ -1,5 +1,8 @@
 //used to denote all available damage types, for immunity/vulnerability/resistance
+//should this be one section with 3 checkboxes for each type?  alternative takes up a lot of space.  
 import React, { Component } from 'react'
 
-const typeList = [Slashing, Piercing, Bludgeoning, Poison, Acid, Fire, Cold, Radiant, Necrotic, Lightning, Thunder, Force, Psychic]
-const damageTypes
+class DamageTypes extends Component{
+    const typeList = [Slashing, Piercing, Bludgeoning, Poison, Acid, Fire, Cold, Radiant, Necrotic, Lightning, Thunder, Force, Psychic];
+    
+}
