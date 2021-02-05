5e1e96a517bdf4e1ddfea88767fe633bf5bf8b7a
sbrevolution5 <Sbrevolution5@aim.com>
2021-02-05 11:13:17 -0500

    I've done enough "preformatting"  now react

--- src/components/DamageTypes.js
+++ src/components/DamageTypes.js
@@ -1,4 +1,5 @@
 //used to denote all available damage types, for immunity/vulnerability/resistance
 import React, { Component } from 'react'
 
+const typeList = [Slashing, Piercing, Bludgeoning, Poison, Acid, Fire, Cold, Radiant, Necrotic, Lightning, Thunder, Force, Psychic]
 const damageTypes
