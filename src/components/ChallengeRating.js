4115f46432f8bae8ea480b7bc0c54c4f4fdccf4d
sbrevolution5 <Sbrevolution5@aim.com>
2021-02-10 09:49:14 -0500

    data linked to form CR

--- src/components/ChallengeRating.js
+++ src/components/ChallengeRating.js
@@ -1,11 +1,15 @@
-import {useState} from 'react'
-const ChallengeRating = ()=>{
+import {useState, useEffect} from 'react'
+const ChallengeRating = ({sendData})=>{
     const {cr, setCr} = useState("")
     const crs = ["0", "1/8", "1/4", "1/2", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30"]
+    const callBackMethod=(value)=>{
+        sendData(value);
+    }   
+    useEffect(()=>{callBackMethod(cr)})
     return(
         <div className="form-group">
           <label htmlFor="crForm">Challenge Rating</label>
-          <select className="form-control bg-dark" name="" id="crForm">
+          <select className="form-control bg-dark" name="" id="crForm" onChange={e=>{setCr(e.target.value)}}>
             {crs.map((value, index)=>{
                 return <option key={index} value={value}>{value}</option>;
             })}
