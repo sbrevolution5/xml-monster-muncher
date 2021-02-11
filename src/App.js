89459539c8447ed8b4b60ec10f3aea72a0bfff0a
sbrevolution5 <Sbrevolution5@aim.com>
2021-02-11 16:09:33 -0500

    Added a readme message to describe functionality at the bottom

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ function App() {
         <h1>XML Monster Muncher</h1>
       </header>
         <Form></Form>
+      <div>
+        <p class="form-text text-muted">
+          Fill in what is needed for your monster, then copy paste into a txt file ending in .xml with a surrounding tag of compendium.  then import into your favorite combat tracker.  Currently optimized for game master 5.
+        </p>
+      </div>
       <footer>
         <a href="https://github.com/sbrevolution5/xml-monster-muncher">Contribute on Github <i className="fa fa-github" aria-hidden="true"></i></a>
         <p><i className="fa fa-copyright" aria-hidden="true"></i> Seth A. Burleson 2021</p>
