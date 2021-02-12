f18dbb76037026f4c87f1b46dd0b3a9012d8aac4
sbrevolution5 <Sbrevolution5@aim.com>
2021-02-12 12:57:23 -0500

    fixed traitcard

--- src/components/TraitCard.js
+++ src/components/TraitCard.js
@@ -1,27 +1,27 @@
-import { Component } from "react";
+import { Component, useState, useEffect } from "react";
+const TraitCard = ({sendTraitData}) => {
+    const [title, setTitle] = useState("")
+    const [text, setText] = useState("")
 
-class TraitCard extends Component {
-    render() {
-        return (
-            <div>
-                {/* remove buton for each card  */}
-                <div className="card text-white bg-dark mb-3 d-inline-block" >
-                    <div className="card-header">Trait Card</div>
-                    <div className="card-body">
-                        <div className="form-group">
-                            <label htmlFor="traitTitle"></label>
-                            <input type="text" className="form-control bg-dark" name="title" id="traitTitle" aria-describedby="helpId" placeholder="" />
-                            <small id="helpId" className="form-text text-muted">Name of trait</small>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="traitDescription"></label>
-                            <textarea className="form-control bg-dark" name="description" id="traitDescription" placeholder="describe trait here" rows="3"></textarea>
-                        </div>
-                        <button type="button" className="btn btn-danger">Remove Trait</button>
+    const callBackMethod=(value)=>{
+        sendTraitData(value);
+    }
+    useEffect(()=>{ callBackMethod({title: title, text: text})}, [title, text])
+    return (
+        <div>
+            {/* remove buton for each card  */}
+            <div className="card text-white bg-dark mb-3 d-inline-block">
+                <div className="card-header"><input type="text" className="form-control bg-dark text-light" name="title" id="traitTitle" aria-describedby="helpId" placeholder="" onChange={(e)=>{setTitle(e.target.value)}}/></div>
+                <small id="helpId" className="form-text text-muted" >Name of trait</small>
+                <div className="card-body">
+                    <div className="form-group">
+                        <label htmlFor="traitDescription"></label>
+                        <textarea className="form-control bg-dark text-light" name="description" id="traitDescription" placeholder="describe aciton here" rows="3"onChange={(e)=>{setText(e.target.value)}}></textarea>
                     </div>
+                    <button type="button" className="btn btn-danger">Remove Trait</button>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
-export default TraitCard;
+export default TraitCard
